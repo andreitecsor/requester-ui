3344a0441a6ef87884a85e64acc1842f88497cb6
Andrei Tecsor <andrei.tecsor@gmail.com>
2023-07-01 23:54:56 +0300

    send the pid and retrieve the score for a single provider

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+D
+public/manifest.json,4/2/42e89cd2213b9d5dfae4f63522702c623170d792
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
+A
+public/robots.txt,d/a/dac799070c2d0c2fa24462b02c1f2dcc3fb3a1e9
+;
+src/App.css,a/a/aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
+<
+src/logo.svg,6/d/6d3a8cd532ece5b4ddbd4b3b6a678ba686d300bc
+F
+src/reportWebVitals.js,1/c/1ce5e63ae1582ed0b425c46001299a07515efbf9
--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [pid, setPid] = useState('');
+    const [score, setScore] = useState(null);
+    let intervalId = null;  // Store the interval ID so you can cancel it later.
+
+    const handleInputChange = (event) => {
+        setPid(event.target.value);
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/requester/api/v1', {
+                pid: pid,
+            });
+            const data = response.data;
+
+            if (data) {
+                // Call the GET request every minute until the status is 'done'.
+                intervalId = setInterval(async () => {
+                    const response = await axios.get(`http://localhost:8080/requester/api/v1/${pid}`);
+                    const data = response.data;
+
+                    if (data.status === 'done') {
+                        clearInterval(intervalId);
+                        setScore(data.score);
+                    }
+                }, 60000);  // 60000 milliseconds = 1 minute
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <TextField
+                label="Input Text"
+                variant="outlined"
+                value={pid}
+                onChange={handleInputChange}
+            />
+            <Box mt={2}>
+                <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
+            </Box>
+            {/* Display the score. */}
+            {score !== null && (
+                <div>Score: {score}</div>
+            )}
+        </Box>
+    );
 }
 
 export default App;
